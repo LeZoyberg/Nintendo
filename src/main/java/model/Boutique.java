86d75625070d7d18fa6fc5a586a55a0b7ded8b7d
blagnel <benjain.lagnel@hotamd.com>
2023-11-09 10:24:13 +0100

    Add class Boutique

---
+++ src/main/java/model/Boutique.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Boutique {
+	private String nom;
+	private String Adresse;
+	public Boutique(String nom, String adresse) {
+		this.nom = nom;
+		Adresse = adresse;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return Adresse;
+	}
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	} 
+	
+}
