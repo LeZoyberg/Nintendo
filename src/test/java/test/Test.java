689510f81aa3683e7b62a983c364a4242cc7db0f
blagnel82 <benjamin.lagnel@hotmail.com>
2023-11-09 11:30:10 +0100

    last commit après on arrete

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,26 +1,30 @@
 package test;
 
+import java.time.LocalDate;
+
+import model.Achat;
 import model.Boutique;
 import model.Console;
 import model.Jeu;
 import model.Client;
+
 public class Test {
 
-	
+
 	public static void main(String[] args) {
+		
 	Console Nintendo64 = new Console("Nintendo64");	
 	Console Wii = new Console("Wii");
 	Console Switch = new Console("Switch");
-	Jeu j1 = new Jeu("SuperMario64",Nintendo64);
-	Jeu j2 = new Jeu("MarioParty",Nintendo64);
-	Jeu j3 = new Jeu("Animal Crossing",Switch);
-	Jeu j4 = new Jeu("Wii Sport",Wii);
-	Jeu j5 = new Jeu("Pokemon Stadium",Nintendo64);
 	Client c1 = new Client("ali","baba");
 	Client c2 = new Client("élé","40Voleur");
-	
 	Boutique b1 = new Boutique("Melee Boutique", "55 Rue de Luigi") ; 
-	
+	Jeu j1 = new Jeu("SuperMario64",Nintendo64,b1);
+	Jeu j2 = new Jeu("MarioParty",Nintendo64,b1);
+	Jeu j3 = new Jeu("Animal Crossing",Switch,b1);
+	Jeu j4 = new Jeu("Wii Sport",Wii,b1);
+	Jeu j5 = new Jeu("Pokemon Stadium",Nintendo64,b1);
+	//c1.getAchats().add(j5,LocalDate.now(),1.0);
 	}
 
 }
