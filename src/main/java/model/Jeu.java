24e3928df670ed88cf6e1bff0b0cc3d8ddfb778d
Ruxey <lorys.legohebel@gmail.com>
2023-11-09 10:42:45 +0100

    Add attribut Boutique in class Jeu

--- src/main/java/model/Jeu.java
+++ src/main/java/model/Jeu.java
@@ -6,14 +6,13 @@ public class Jeu {
 
 	private String titre;
 	private Console console;
+	private Boutique boutique;
 	
-	
-	
-	public Jeu(String titre, Console console) {
+	public Jeu(String titre, Console console, Boutique boutique) {
 		this.titre = titre;
 		this.console = console;
+		this.boutique = boutique;
 	}
-
 
 
 	public String getTitre() {
@@ -40,9 +39,19 @@ public class Jeu {
 
 
 
+	public Boutique getBoutique() {
+		return boutique;
+	}
+
+
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Jeu [titre=" + titre + ", console=" + console + "]";
+		return "Jeu [titre=" + titre + ", console=" + console + ", boutique=" + boutique + "]";
 	}
 	
 	
