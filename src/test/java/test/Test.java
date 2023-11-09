28a08c9c146816da2dd0135244c5d0c4cc04366a
LeZoyberg <pierre.langlade@outlook.fr>
2023-11-09 10:26:26 +0100

    Add 5 jeux dans main

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,10 +1,23 @@
 package test;
 
+import model.Console;
+import model.Jeu;
+
 public class Test {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Console Nintendo64 = new Console("Nintendo64");	
+	Console Wii = new Console("Wii");
+	Console Switch = new Console("Switch");
+	Jeu j1 = new Jeu("SuperMario64",Nintendo64);
+	Jeu j2 = new Jeu("MarioParty",Nintendo64);
+	Jeu j3 = new Jeu("Animal Crossing",Switch);
+	Jeu j4 = new Jeu("Wii Sport",Wii);
+	Jeu j5 = new Jeu("Pokemon Stadium",Nintendo64);
+	
+	
+	
 	}
 
 }
